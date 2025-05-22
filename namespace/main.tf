b7e7beacbeab3291948dc73e12e7bfa77dd0fa7c
dungphung411 <mdungphung411@gmail.com>
2025-05-22 10:47:55 +0700

    add ns

---
+++ namespace/main.tf
@@ -0,0 +1,18 @@
+#Basic example for namespace
+resource "rafay_namespace" "oach" {
+  metadata {
+    name    = "oachoach"
+    project = "mdpmdp"
+  }
+  spec {
+    drift {
+      enabled = false
+    }
+    placement {
+      labels {
+        key   = "rafay.dev/clusterName"
+        value = "oach"
+      }
+    }
+  }
+}
