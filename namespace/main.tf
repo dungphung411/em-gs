77049a74937f60310631de0b1dc75e89938442f5
dungphung411 <mdungphung411@gmail.com>
2025-05-22 11:20:29 +0700

    yep

--- namespace/main.tf
+++ namespace/main.tf
@@ -1,20 +1,7 @@
-terraform {
-  required_providers {
-    rafay = {
-      version = ">= 0.1"
-      source  = "RafaySystems/rafay"
-    }
-  }
-}
 
-provider "rafay" {
-  provider_config_file = "D:/PROJECT/envmgmr/config.json"
-}
-
-#Basic example for namespace
 resource "rafay_namespace" "oach" {
   metadata {
-    name    = "oach"
+    name    = "oach1"
     project = "mdp"
   }
   spec {
